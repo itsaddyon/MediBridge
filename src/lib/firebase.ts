850f6b25f16be3785339e07cbc37b3bbecb8b2e2
itsaddyon <itsaddyon@gmail.com>
2025-12-22 05:33:29 +0530

    Minor Fixes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXOpeb6Phmuwpapl07mi70Quy8lgg-0y8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "medibridge-cbe84.firebaseapp.com",
   projectId: "medibridge-cbe84",
   storageBucket: "medibridge-cbe84.firebasestorage.app",
   messagingSenderId: "884589594368",
   appId: "1:884589594368:web:ce8d8d093dffe8be3715e5",
 };
+
 
 const app = initializeApp(firebaseConfig);
 
